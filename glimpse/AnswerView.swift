342bfabd78e3f006c6d5fc6dd8b199a93be37870
Jimmy DeLano <jimmydelano91@gmail.com>
2023-10-30 22:26:25 -0700

    next steps

--- glimpse/AnswerView.swift
+++ glimpse/AnswerView.swift
@@ -8,3 +8,9 @@
 import SwiftUI
 import SwiftData
 
+// TODO: pick up here.
+// view to display questions --> y/n pickers
+// enter into modelContext save somehow
+// add as button for now in Content View
+// block if question has already been answered today
+// --> add notificaitons to trigger this view to show up
